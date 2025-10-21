8a64dc2a41316260a00673e52ebefa63ba81c48a
Aizaz Soomro <aizazsoomro9@gmail.com>
2025-10-21 19:04:11 +0500

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-            I am a full-stack developer with over 2 years of experience in
+            I am a full-stack developer with over 6 months of experience in
             building scalable web applications. Skilled in both front-end and
             back-end development, I specialize in the MERN stack and other
             modern technologies to create seamless user experiences and
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/15MZdE3ubXK2sCds_X5T9ZPqYmP7nrQdC/view?usp=drive_link"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
